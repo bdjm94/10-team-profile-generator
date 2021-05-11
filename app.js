fe3707f0128821ad120589008db2b32abd0cd5fa
bdjm <brendandjmoore@gmail.com>
2021-05-11 22:10:50 +1000

    adding the script for the employee questions

--- app.js
+++ app.js
@@ -9,3 +9,34 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+
+let employees = [];
+
+const employeeQs = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: 'Please select the employee type from the below.',
+            name: 'employeeType',
+            choices: [Manager, Engineer, Intern]
+        },
+    ])
+        .then(answer => {
+
+            if (answer.employeeType === 'Manager') {
+                managerQs();
+            } else if
+                (answer.employeeType === 'Engineer') {
+                engineerQs();
+            } else if
+                (answer.employeeType === 'Intern') {
+                internQs();
+            }
+            else {
+                console.log('Employee type has been selected!');
+                return;
+            }
+        })
+}
+
+employeeQsS();
