80f66339226771531018f133c7e1363272dafc62
bdjm <brendandjmoore@gmail.com>
2021-05-11 22:15:10 +1000

    adding the manager questions

--- app.js
+++ app.js
@@ -40,3 +40,49 @@ const employeeQs = () => {
 }
 
 employeeQs();
+
+const managerQs = () => {
+    inquirer.prompt([
+
+        {
+            type: 'input',
+            message: 'What is the name of the Manager?',
+            name: 'managerName'
+        },
+        {
+            type: 'input',
+            message: 'What is the Managers ID number?',
+            name: 'id',
+        },
+        {
+            type: 'input',
+            message: 'What is the Managers email?',
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: 'What is the Managers office number?',
+            name: 'officeNumber',
+        },
+        {
+            type: 'confirm',
+            message: 'Would you like to add another employee?',
+            name: 'addCheck',
+        },
+
+    ])
+        .then(answers => {
+
+            const manager = new Manager(answers.managerName, answers.id, answers.email, answers.officeNumber);
+            employees.push(manager);
+
+            console.log(employees);
+
+            if (answers.addCheck) {
+                initialQuestion();
+            } else {
+                let data = render(employees);
+                fs.writeFile(outputPath, data, (err) => {
+                    if (err) throw err;
+                    console.log('Success! Managers file has been saved!');
+                });
